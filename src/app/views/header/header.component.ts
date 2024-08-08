64e0292ab7d390d66a7cd71cae8065adf86c52c2
WinterABW <skarnanotherblackwinter@gmail.com>
2024-08-07 17:48:01 -0700

    Update header and per-hour components with improved styling and image paths

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component} from '@angular/core';
+import { PerHourComponent } from '../../common/components/per-hour/per-hour.component';
+import { LevelComponent } from '../../common/components/level/level.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [PerHourComponent,LevelComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
