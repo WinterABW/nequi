1790d427ae0650564e92076399aeaa7a2ae9ef52
WinterABW <skarnanotherblackwinter@gmail.com>
2024-08-07 17:54:45 -0700

    Update background color in main and mining components

--- src/app/views/mining/mining.component.ts
+++ src/app/views/mining/mining.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PerHourComponent } from '../../common/components/per-hour/per-hour.component';
+import { LevelComponent } from '../../common/components/level/level.component';
 
 @Component({
   selector: 'app-mining',
   standalone: true,
-  imports: [],
+  imports: [PerHourComponent,LevelComponent],
   templateUrl: './mining.component.html',
   styleUrl: './mining.component.scss'
 })
