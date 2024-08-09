857ac8de7079583c7fef6152d9bf965be783eac0
WinterABW <skarnanotherblackwinter@gmail.com>
2024-08-08 23:42:02 -0700

    feat: Update level component to display progress and gradient colors dynamically

--- src/app/common/components/level/level.component.ts
+++ src/app/common/components/level/level.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-level',
   standalone: true,
-  imports: [],
+  imports: [NgStyle,NgClass],
   templateUrl: './level.component.html',
-  styleUrl: './level.component.scss'
+  styleUrl: './level.component.scss',
 })
 export class LevelComponent {
-
+  @Input() progress: number = 0;
+  @Input() gradientColors: string[] = ['#756475', '#4c104e'];
 }
