736822ee801737a098de05c282304ca33c0bf265
WinterABW <skarnanotherblackwinter@gmail.com>
2024-08-09 01:46:58 -0700

    chore: Add toast component to mining view

--- src/app/views/mining/mining.component.ts
+++ src/app/views/mining/mining.component.ts
@@ -5,13 +5,15 @@ import { RouterLink } from '@angular/router';
 import { BalanceComponent } from '../../common/components/balance/balance.component';
 import { ProductComponent } from './components/product/product.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ToastComponent } from './components/toast/toast.component';
 
 const components = [
   PerHourComponent,
   LevelComponent,
   BalanceComponent,
   ProductComponent,
-  NavComponent
+  NavComponent,
+  ToastComponent,
 ];
 
 @Component({
