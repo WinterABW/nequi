1abc8228d98a99afaa6c497ee08b8d205e533635
WinterABW <skarnanotherblackwinter@gmail.com>
2024-08-08 23:55:10 -0700

    feat: Add navigation component to mining view

--- src/app/views/mining/mining.component.ts
+++ src/app/views/mining/mining.component.ts
@@ -4,12 +4,14 @@ import { LevelComponent } from '../../common/components/level/level.component';
 import { RouterLink } from '@angular/router';
 import { BalanceComponent } from '../../common/components/balance/balance.component';
 import { ProductComponent } from './components/product/product.component';
+import { NavComponent } from './components/nav/nav.component';
 
 const components = [
   PerHourComponent,
   LevelComponent,
   BalanceComponent,
   ProductComponent,
+  NavComponent
 ];
 
 @Component({
