9b99d74bf18637e9d710238c31f5eeb94636da3a
Victor Manuel Avila Tamayo <108910493+WinterABW@users.noreply.github.com>
2024-08-13 09:37:08 -0400

    anim

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
     loadComponent: () =>
       import('./views/main/main.component').then((c) => c.MainComponent),
   },
